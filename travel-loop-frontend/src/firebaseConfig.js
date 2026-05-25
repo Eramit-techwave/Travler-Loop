a59c5a10c8c8bf4124c84d0bdc23f5fc61c449ca
Amit Dubey <amitdubey04102004@gmail.com>
2026-05-25 23:25:49 +0530

    NameUpdate Travel-loop to travel india

--- travel-loop-frontend/src/firebaseConfig.js
+++ travel-loop-frontend/src/firebaseConfig.js
@@ -3,9 +3,9 @@ import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBW2K1T17efLvjSyVUAuH6DrXReeP_fCt4",
-  authDomain: "travelloop-3665d.firebaseapp.com",
-  projectId: "travelloop-3665d",
-  storageBucket: "travelloop-3665d.firebasestorage.app",
+  authDomain: "TravelIndia -3665d.firebaseapp.com",
+  projectId: "TravelIndia -3665d",
+  storageBucket: "TravelIndia -3665d.firebasestorage.app",
   messagingSenderId: "30915691226",
   appId: "1:30915691226:web:0a7fdee3aaed1cf5bfdbe3"
 };
